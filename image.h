1d07a897321bb9468e7440444217715e38ae5320
victorygc <victorygarciac@gmail.com>
2017-11-14 14:20:46 -0400

    Se guarda y carga la informacion de la imagen en un archivo bianrio,  incluye el vectro r24 asociado.

--- image.h
+++ image.h
@@ -10,6 +10,7 @@
 #ifndef IMAGE_H
 #define IMAGE_H
 
+#define maxKeypoints 200
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
@@ -27,14 +28,22 @@ struct img_data{
     int index;
     int year;
     int n_labels;
-    struct keyPoint* key_Point;
+    struct keyPoint key_Point[maxKeypoints];
 };
 
 void testTxt(void);
 
 struct img_data getDescriptor(std::string fileName,cv::Mat img_MR, int year, int index);
+// Load the information of all the clasification point from a text file, and associate 
+// an r24 vector from the Maximun Response filter.
 
 void saveDescriptor(struct img_data data);
+// Save the entire information of an image, include the coordinate, it's label
+// and the associated r24 vector from the Maximum Response filter in a binary file.
+
+struct img_data loadDescriptor();
+// Load the entire information of an image, include the coordinate, it's label
+// and the associated r24 vector from a binary file.
 
 void getDictionaryTextons(cv::Mat dictionaryTextons, struct img_data data[200], int start_index, int finish_index);
 
--- main.cpp
+++ main.cpp
@@ -18,9 +18,9 @@
 
 int main() {
     // Creating the structures for the 2008, 2009 and 2010 sets
-    struct img_data data_2008[671];
-    struct img_data data_2009[695];
-    struct img_data data_2010[689];    
+    //struct img_data data_2008[671];
+    //struct img_data data_2009[695];
+    //struct img_data data_2010[689];    
 
     // Reading the image.
     cv::Mat img = cv::imread("./Vision_MCR/2008/mcr_lter1_fringingreef_pole1-2_qu1_20080415.jpg",CV_LOAD_IMAGE_COLOR);
@@ -49,19 +49,26 @@ int main() {
     cv::Mat img_MR(img_Lab.size().height,img_Lab.size().width, CV_32FC(24));
     maximumResponseFilter(img_Lab, img_MR, kernel);
 
+    // Get the entire data from image text file (coord, label, and r24 vector from img_MR) 
+    // and stores in img_data struct
     std::string fileName = "./Vision_MCR/2008/mcr_lter1_fringingreef_pole1-2_qu1_20080415.jpg.txt";
     struct img_data data = getDescriptor(fileName, img_MR, 2008, 1);
-    //readtxt(fileName, data, 2008, 1);
+    
+    // save an image data in a binary file
+    saveDescriptor(data);
 
-    // Impresion de prueba
+    // load the image data from a binary file
+    struct img_data data = loadDescriptor();
+
+    // print test, first the coordinates and labels, next an r24 vector information
     for(int i = 0; i<data.n_labels ;i++){
         std::cout << data.key_Point[i].pt.x << " " << data.key_Point[i].pt.y << " " << data.key_Point[i].type << std::endl;
     }
     for(int i = 0; i<24 ;i++){
         std::cout << data.key_Point[i].r24[i] << std::endl;
     }
-    // Creating diccionary of textons
+    
     cv::Mat dictionaryTextons(135, 24, CV_32FC1);
-    delete [] data.key_Point;
+
     return 0;
 }
